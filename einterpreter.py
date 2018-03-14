1bc3222ab082f0a3a2980bca907539d20f73a88d
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-03-13 20:44:10 -0500

    Update einterpreter.py

--- einterpreter.py
+++ einterpreter.py
@@ -1,3 +1,5 @@
+import sys
+
 def run(code):
     codes = code.split(' ')
     ret = ""
@@ -6,3 +8,21 @@ def run(code):
         char = chr(length)
         ret += char
     return ret
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1].endswith(".e"):
+            file = sys.argv[1]
+            try:
+                with open(file, 'r') as f:
+                    code = file.read()
+                ran = run(code)
+                print(ran)
+            except:
+                print("Invalid file.")
+                sys.exit(1)
+        else:
+            print("Invalid file.")
+            sys.exit(1)
+    else:
+        print("No file specified.")
