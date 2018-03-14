17e06351651940f5ea6452d58be9bbffe9fcfe11
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-03-13 20:36:00 -0500

    Add files via upload

---
+++ Compiler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Compiler
+{
+    public class Compiler
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("No file to compile.");
+                return 1;
+            }
+            else if (args.Length == 1)
+            {
+                if (args[0].EndsWith(".e"))
+                {
+                    // Compile the file
+                    
+                    string text = System.IO.File.ReadAllText(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), args[0]));
+                    
+                    string[] split = text.Split(null);
+                    foreach(string str in split)
+                    {
+                        int len = str.Length;
+                        char character = (char)len;
+                        System.Console.Write(character);
+                    }
+                    return 1;
+                }
+                else
+                {
+                    System.Console.WriteLine("Invalid input file.");
+                    return 1;
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid input file.");
+                return 1;
+            }
+        }
+    }
+}
